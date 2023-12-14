a46edefd92d11c81d7e3c607c1bf7b214edc108b
Christopher Eadie <caeadie@hotmail.com>
2023-12-14 16:07:30 +0000

    updated build

--- docs/main.js
+++ docs/main.js
@@ -40627,6 +40627,28 @@ if (false) {} else {
 
 /***/ }),
 
+/***/ "./src/apis/apicontroller.ts":
+/*!***********************************!*\
+  !*** ./src/apis/apicontroller.ts ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   ApiController: () => (/* binding */ ApiController)
+/* harmony export */ });
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/lib/axios.js");
+
+class ApiController {
+    constructor() {
+        axios__WEBPACK_IMPORTED_MODULE_0__["default"].defaults.headers.common['ngrok-skip-browser-warning'] = 'latest';
+    }
+    apiRoot = 'https://a306-78-149-128-70.ngrok-free.app';
+}
+
+
+/***/ }),
+
 /***/ "./src/apis/weatherapi/weatherapi.ts":
 /*!*******************************************!*\
   !*** ./src/apis/weatherapi/weatherapi.ts ***!
@@ -40638,61 +40660,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   WeatherApi: () => (/* binding */ WeatherApi)
 /* harmony export */ });
 /* harmony import */ var _models_ApiResponseModels__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../models/ApiResponseModels */ "./src/models/ApiResponseModels.ts");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/lib/axios.js");
+/* harmony import */ var _apicontroller__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../apicontroller */ "./src/apis/apicontroller.ts");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! axios */ "./node_modules/axios/lib/axios.js");
 
 
-class WeatherApi {
-    apiKey = "79403c00176ee7f4e71870efcfdd372b";
-    lat = "55.8554082";
-    lon = "-4.3152956";
-    apiRoot = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}`;
-    constructor() { }
+
+class WeatherApi extends _apicontroller__WEBPACK_IMPORTED_MODULE_1__.ApiController {
+    constructor() {
+        super();
+    }
     async getCurrentForcast() {
-        const result = await axios__WEBPACK_IMPORTED_MODULE_1__["default"].get(this.apiRoot);
+        const result = await axios__WEBPACK_IMPORTED_MODULE_2__["default"].get(`${this.apiRoot}/api/weather`);
         return new _models_ApiResponseModels__WEBPACK_IMPORTED_MODULE_0__.Response(result.data);
-        // return new Response({
-        //     "coord": {
-        //         "lon": -4.3153,
-        //         "lat": 55.8554
-        //     },
-        //     "weather": [
-        //         {
-        //             "id": 802,
-        //             "main": "Clouds",
-        //             "description": "scattered clouds",
-        //             "icon": "03n"
-        //         }
-        //     ],
-        //     "base": "stations",
-        //     "main": {
-        //         "temp": 277.83,
-        //         "feels_like": 276.68,
-        //         "temp_min": 276.77,
-        //         "temp_max": 278.12,
-        //         "pressure": 1011,
-        //         "humidity": 84
-        //     },
-        //     "visibility": 10000,
-        //     "wind": {
-        //         "speed": 1.54,
-        //         "deg": 100
-        //     },
-        //     "clouds": {
-        //         "all": 40
-        //     },
-        //     "dt": 1702483632,
-        //     "sys": {
-        //         "type": 2,
-        //         "id": 47674,
-        //         "country": "GB",
-        //         "sunrise": 1702456759,
-        //         "sunset": 1702482204
-        //     },
-        //     "timezone": 0,
-        //     "id": 2648579,
-        //     "name": "Glasgow",
-        //     "cod": 200
-        // })
     }
 }
 
@@ -40869,7 +40848,7 @@ function WeatherComponent() {
     });
     (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {
         weatherService.getCurrentForcast().then((res) => {
-            let weather = res.data.weather[0];
+            let weather = res.data;
             setState({
                 desc: weather.description,
                 id: weather.id
