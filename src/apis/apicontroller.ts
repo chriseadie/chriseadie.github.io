ac9d2b086b5f452dcde0bdec868369cb70c9fad0
Christopher Eadie <caeadie@hotmail.com>
2023-12-14 17:36:46 +0000

    update ngrok domain

--- src/apis/apicontroller.ts
+++ src/apis/apicontroller.ts
@@ -5,5 +5,5 @@ export class ApiController {
     constructor(){
         axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'latest';
     }
-    public apiRoot:string = 'https://a306-78-149-128-70.ngrok-free.app'
+    public apiRoot:string = 'https://b536-78-149-128-70.ngrok-free.app'
 }
