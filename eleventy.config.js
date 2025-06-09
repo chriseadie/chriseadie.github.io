eca9d29862573fb7ef1608841979571baca16b13
Chris Eadie <caeadie@hotmail.com>
2025-06-09 13:52:18 +0100

    my work page & updates to about page

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,8 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/public/js")
     eleventyConfig.addPassthroughCopy("./src/public/fonts")
     eleventyConfig.addPassthroughCopy("./src/public/imgs")
+
+    eleventyConfig.addNunjucksFilter("log", (val) => console.log(val));
     
     return {
         dir: {
